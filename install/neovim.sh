cf7c4ac7aab07cd9ee5e124c6b4becbd39716bd3
TakuroSugahara <k5690033@gmail.com>
2022-09-16 13:28:47 +0900

    update

--- install/neovim.sh
+++ install/neovim.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 
 apt-get update -y && \
   apt-get install -y software-properties-common && \
--- install/node.sh
+++ install/node.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+set -ex
 
 sudo apt install -y nodejs npm && \
   npm install n -g && \

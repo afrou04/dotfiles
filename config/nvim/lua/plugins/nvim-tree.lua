7f8e1f6768e718a1b4c4bfab62f11eb1fabf7f27
TakuroSugahara <k5690033@gmail.com>
2023-11-05 14:55:20 +0900

    nvim-treeのアプデに合わせて設定ファイルの修正

--- config/nvim/lua/plugins/nvim-tree.lua
+++ config/nvim/lua/plugins/nvim-tree.lua
@@ -1,95 +1,34 @@
-local tree_cb = require'nvim-tree.config'.nvim_tree_callback
-local mapping_list = {
-  { key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb("edit") },
-  { key = {"<2-RightMouse>", "<C-]>"},    cb = tree_cb("cd") },
-  { key = "<C-v>",                        cb = tree_cb("vsplit") },
-  { key = "<C-x>",                        cb = tree_cb("split") },
-  { key = "<Tab>",                        cb = tree_cb("preview") },
-  { key = "nf",                            cb = tree_cb("create") },
-  { key = "nd",                            cb = tree_cb("create") },
-  { key = "rm",                            cb = tree_cb("remove") },
-  { key = "rn",                            cb = tree_cb("rename") },
-  { key = "co",                            cb = tree_cb("copy") },
-  { key = "pp",                            cb = tree_cb("paste") },
-  { key = "yy",                            cb = tree_cb("copy_name") },
-  { key = "YY",                            cb = tree_cb("copy_path") },
-  { key = "gy",                           cb = tree_cb("copy_absolute_path") },
-  { key = "s",                            cb = tree_cb("system_open") },
-  { key = "q",                            cb = tree_cb("close") },
-  { key = "g?",                           cb = tree_cb("toggle_help") },
-}
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
--- setup
-require'nvim-tree'.setup {
-  disable_netrw       = true,
-  hijack_netrw        = true,
-  open_on_setup       = false,
-  ignore_ft_on_setup  = {},
-  open_on_tab         = false,
-  hijack_cursor       = false,
-  update_cwd          = false,
+local function my_on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  api.config.mappings.default_on_attach(bufnr)
+  vim.keymap.del('n', '<C-e>', { buffer = bufnr })
+end
+
+require("nvim-tree").setup({
+  on_attach = my_on_attach,
+  view = {
+    width = 40,
+  },
+  filters = {
+    git_ignored = false,
+  },
   actions = {
-    use_system_clipboard = true,
-    change_dir = {
-      enable = true,
-      global = false,
-      restrict_above_cwd = false,
-    },
     open_file = {
-      quit_on_open = true,
-      resize_window = true,
       window_picker = {
-        enable = false,
-        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+        enable = true,
+        picker = "default",
+        chars = "1234567890",
         exclude = {
-          filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+          filetype = { "notify", "packer", "qf", "diff" },
           buftype = { "nofile", "terminal", "help" },
         },
       },
     },
   },
-  diagnostics = {
-    enable = false,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    }
-  },
-  update_focused_file = {
-    enable      = true,
-    update_cwd  = false,
-    ignore_list = {}
-  },
-  system_open = {
-    cmd  = nil,
-    args = {}
-  },
-  filters = {
-    dotfiles = false,
-    custom = {}
-  },
-  git = {
-    enable = true,
-    ignore = true,
-    timeout = 500,
-  },
-  view = {
-    width = 40,
-    hide_root_folder = false,
-    side = 'left',
-    mappings = {
-      custom_only = false,
-      list = mapping_list
-    },
-    number = false,
-    relativenumber = false,
-    signcolumn = "yes"
-  },
-  trash = {
-    cmd = "trash",
-    require_confirm = true
-  }
-}
+})
 
